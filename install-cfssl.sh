b9ce03118b384b9472a794f4c446f600531c1e94
ethan <konmyn@163.com>
2019-03-18 18:22:46 +0800

    add install cfssl script

---
+++ install-cfssl.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+mkdir ~/bin
+curl -s -L -o ~/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+curl -s -L -o ~/bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+chmod +x ~/bin/{cfssl,cfssljson}
+export PATH=$PATH:~/bin
